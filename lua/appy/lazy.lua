ffbefcab958b79ef89da692899cc2802849c4325
Amandeep Yadav <amandeepyadav79@gmail.com>
2026-06-21 21:22:21 +0530

    configure lsp

--- lua/appy/lazy.lua
+++ lua/appy/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("appy.plugins", {
+require("lazy").setup({ { import = "appy.plugins" } , { import = "appy.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
